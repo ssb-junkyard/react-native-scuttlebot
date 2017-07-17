62c43c4b6ff02f397807bd32e5e91a2df7013c82
Andre Staltz <andre@staltz.com>
2017-07-17 14:10:15 +0300

    Make insert-bundle.js more resilient, support npm i

--- insert-bundle.js
+++ insert-bundle.js
@@ -1,20 +1,26 @@
 const path = require("path");
 const execSync = require("child_process").execSync;
+const fs = require("fs");
 
+const innerBinsPath = path.join(__dirname, "node_modules/.bin/");
+const outerBinsPath = path.join(__dirname, "../.bin/");
+let noderifyBinPath = path.join(innerBinsPath, "noderify");
+if (!fs.existsSync(noderifyBinPath)) {
+  noderifyBinPath = path.join(outerBinsPath, "noderify");
+}
+let rnnodeBinPath = path.join(innerBinsPath, "react-native-node");
+if (!fs.existsSync(rnnodeBinPath)) {
+  rnnodeBinPath = path.join(outerBinsPath, "react-native-node");
+}
 const projectPath = path.join(__dirname, "../../");
-const noderifyBinPath = path.join(__dirname, "./node_modules/.bin/noderify");
-const rnnodeBinPath = path.join(
-  __dirname,
-  "./node_modules/.bin/react-native-node"
-);
 const replacements =
   "--replace.chloride=sodium-browserify-tweetnacl " +
   "--replace.sodium-chloride=sodium-browserify-tweetnacl " +
   "--replace.node-extend=xtend " +
   "--replace.leveldown=memdown";
-const ssbPeerPath = path.join(__dirname, "./ssb-peer.js");
-const bundleFilePath = path.join(__dirname, "./bundle.js");
-const bundleDirPath = path.join(__dirname, "../../background-bundled");
+const ssbPeerPath = path.join(__dirname, "ssb-peer.js");
+const bundleFilePath = path.join(__dirname, "bundle.js");
+const bundleDirPath = path.join(projectPath, "background-bundled");
 
 execSync(
   `${noderifyBinPath} ${replacements} ${ssbPeerPath} > ${bundleFilePath}`,

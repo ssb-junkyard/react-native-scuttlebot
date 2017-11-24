cf886ed2d4e7ca355e34696139863570322e043e
Andre Staltz <andre@staltz.com>
2017-11-24 22:36:23 +0200

    Convert insert-bundle script from js to bash

---
+++ insert-bundle.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+INNER_BINS="$(pwd)/node_modules/.bin"
+OUTER_BINS="$(pwd)/../.bin"
+NODERIFY="$INNER_BINS/noderify"
+if [ ! -e "$NODERIFY" ]; then
+  NODERIFY="$OUTER_BINS/noderify"
+fi
+RNNODE="$INNER_BINS/react-native-node"
+if [ ! -e "$RNNODE" ]; then
+  RNNODE="$OUTER_BINS/react-native-node"
+fi
+PROJECT="$(pwd)/../.."
+ORIG="$(pwd)/rnnodeapp"
+DEST="$PROJECT/rnnodeapp"
+
+# Set up destination
+rm -rf "$DEST";
+mkdir -p "$DEST";
+cp -r "$ORIG" "$DEST/../";
+
+# Install dependencies there
+cd "$DEST";
+yarn;
+
+# Minify the bundle
+cd "$DEST";
+$NODERIFY \
+   --replace.chloride=sodium-browserify-tweetnacl \
+   --replace.sodium-chloride=sodium-browserify-tweetnacl \
+   --replace.node-extend=xtend \
+   --replace.leveldown=leveldown-android-prebuilt \
+   "$DEST/index.js" > "$DEST/_index.js";
+rm "$DEST/index.js";
+mv "$DEST/_index.js" "$DEST/index.js";
+
+# Setup native libraries
+PREBUILT_PATH_1="$DEST/node_modules/leveldown-android-prebuilt/compiled"
+cp -R "$PREBUILT_PATH_1" "$DEST/";
+
+# Pre-insert clean up
+rm -rf "$DEST/node_modules";
+
+# Insert bundle into the project
+cd $PROJECT;
+$RNNODE insert "$DEST";
+
+# Post-insert clean up
+#`rm -rf ${dest}`
